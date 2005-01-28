dff34e5368f7fcc419d911442d30ecea7940b297
forax <forax>
2005-01-28 14:36:59 +0000

    Initial revision

---
+++ groupz/groupz/src/fr/umlv/groupz/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Created on 28 janv. 2005
+ *
+ */
+package fr.umlv.groupz;
+
+import java.util.Arrays;
+
+/**
+ * @author remi
+ *
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(args));
+  }
+}
