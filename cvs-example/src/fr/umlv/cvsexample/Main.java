a8df95132a6ddee23ed29ba9045839d9b5a64a42
forax <forax>
2005-01-28 13:46:34 +0000

    *** empty log message ***

---
+++ cvs-example/src/fr/umlv/cvsexample/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Created on 28 janv. 2005
+ *
+ */
+package fr.umlv.cvsexample;
+
+/**
+ * @author remi
+ *
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hello CVS");
+  }
+}
