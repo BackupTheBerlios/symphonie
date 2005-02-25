aa21ab8f6714cd2c800e37ccced27228230628ff
forax <forax>
2005-02-25 11:01:57 +0000

    class test

---
+++ groupey/src/fr/umlv/groupey/Test2.java
@@ -0,0 +1,19 @@
+/*
+ * Created on 25 févr. 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package fr.umlv.groupey;
+
+/**
+ * @author jvarin
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Test2 {
+
+	public static void main(String[] args) {
+	}
+}
