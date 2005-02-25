e76876f36577b1e16082c85e5d9a4a3a0295708f
forax <forax>
2005-02-25 11:10:01 +0000

    *** empty log message ***

--- groupey/src/fr/umlv/groupey/Test2.java
+++ groupey/src/fr/umlv/groupey/Test2.java
@@ -1,16 +1,12 @@
 /*
  * Created on 25 févr. 2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package fr.umlv.groupey;
 
 /**
  * @author jvarin
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class Test2 {
 	
@@ -21,6 +17,10 @@ public class Test2 {
 	}
 	
 	
+
+	public void bonjour2() {
+	}
+
 
 	public static void main(String[] args) {
 	}
