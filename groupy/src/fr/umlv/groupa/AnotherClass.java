3c4685eb7f4166c88af59f6e8639785751542c6c
forax <forax>
2005-02-25 11:12:38 +0000

    add file

---
+++ groupy/src/fr/umlv/groupa/AnotherClass.java
@@ -0,0 +1,18 @@
+/**
+ * Created on 25 févr. 2005
+ *@author jsie
+ */
+package fr.umlv.groupa;
+
+
+public class AnotherClass {
+/**
+ * $Id: AnotherClass.java,v 1.1 2005/02/25 11:12:38 forax Exp $
+ * $Date: 2005/02/25 11:12:38 $
+ * $Author: forax $
+ * @param args command line parameters
+ */
+    public static void main(String[] args) {
+        System.out.println("another class");
+    }
+}
