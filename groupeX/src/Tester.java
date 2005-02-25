9817b8cdbc465a1e11630e6ca9285f4a470754f3
forax <forax>
2005-02-25 11:19:48 +0000

    *** empty log message ***

--- groupeX/src/Tester.java
+++ groupeX/src/Tester.java
@@ -8,8 +8,6 @@
 /**
  * @author crodri05
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class Tester {
 
---
+++ groupet/src/fr/umlv/groupet/Empty.java
@@ -0,0 +1,14 @@
+package fr.umlv.groupet;
+
+
+public class Empty {
+ private int x;
+
+public Empty(){
+ 	this.x=0;
+ }
+public int getX(){
+	return x;
+	}
+
+}
